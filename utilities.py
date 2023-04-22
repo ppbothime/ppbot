a5b47b7c20e7941e7b597eaf787d5555a127ec83
ppbothime <131559800+ppbothime@users.noreply.github.com>
2023-04-22 23:30:36 +0100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -2,15 +2,14 @@ import random
 from auth import *
 
 def getRandomFile():
-
     drive = getGoogleAuth()
-
     print("GETTING A FILE")
-
-    file_list = drive.ListFile().GetList()
-
+    # Filter file_list to only include files with an `.mkv` extension
+    file_list = drive.ListFile({'q': "title contains '.mkv' and trashed=false"}).GetList()
+    # If no files with `.mkv` extension found, return None
+    if not file_list:
+        return None
     file = random.choice(file_list)
-
     title = file['title']
     extension = title.split(".")[1]
 
